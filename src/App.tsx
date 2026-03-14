4ed1395d650097771e442392872db57b39c0bacc
Julianjaguilar <Julian.206@icloud.com>
2026-03-13 20:22:20 -0700

    Initial Equinox Landscape website

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import HomePage from "./pages/HomePage";
 import ServicePage from "./pages/ServicePage";
 import ContactPage from "./pages/ContactPage";
 
+function ScrollToTop() {
+  const { pathname, hash } = useLocation();
+  useEffect(() => {
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services/:slug" element={<ServicePage />} />
